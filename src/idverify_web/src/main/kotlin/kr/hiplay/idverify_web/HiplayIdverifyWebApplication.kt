35ba3689c5d8c11c24efcffaf84942ccb6e52e6e
kms0219kms <kms0219kms@naver.com>
2024-07-04 22:23:43 +0700

    :rocket: chore: Add Docker compile process
    TODO: Toss, Unified 작업(2단계)

--- src/idverify_web/src/main/kotlin/kr/hiplay/idverify_web/HiplayIdverifyWebApplication.kt
+++ src/idverify_web/src/main/kotlin/kr/hiplay/idverify_web/HiplayIdverifyWebApplication.kt
@@ -1,10 +1,16 @@
 package kr.hiplay.idverify_web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class HiplayIdverifyWebApplication
+class HiplayIdverifyWebApplication : SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(HiplayIdverifyWebApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<HiplayIdverifyWebApplication>(*args)
